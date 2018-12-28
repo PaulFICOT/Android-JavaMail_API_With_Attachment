fb59291ad62e27dea8cc9238fdccf981709a5ae0
Paul FICOT <33148717+PaulFICOT@users.noreply.github.com>
2018-12-28 15:56:37 +0100

    a

---
+++ app/src/main/java/fr/paul_ficot/android_javamail_api_with_attachment/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.paul_ficot.android_javamail_api_with_attachment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
